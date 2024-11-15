5cf0cec5a6c8ce9c7f53104f34dc99fd01637d36
davidgm26 <davidgama260402@gmail.com>
2024-11-15 20:58:52 +0100

    Seguridad implementada, a falta de probar correcta conexion con back

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginRequest } from '../../interfaces/login-request';
+import { AuthService } from '../../services/auth.service';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, 
+    MatButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  hidePassword: boolean = true;
+
+  loginRequest!: LoginRequest;
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(
+    private authService: AuthService,
+  ) { }
+
+
+    onSubmit() {
+      this.loginRequest = this.loginForm.value;
+      console.log(this.loginRequest);
+      this.authService.login(this.loginRequest).subscribe(
+        (resp) => {
+        localStorage.setItem('token',resp.token);
+        },
+        (error)=> {
+          console.error(error);
+        })
+    }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { LoginRequest } from '../interfaces/login-request';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../interfaces/usuario';
+import { environment } from '../../env/environment';
+import { LoginResponse } from '../interfaces/login-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  login(loginRequest: LoginRequest): Observable<LoginResponse> {  
+    return this.http.post<LoginResponse>(`${environment.apiUrl}login`,loginRequest);
+  }
+}
