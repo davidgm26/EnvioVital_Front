c84cb7d5abee909b89044c21341241ccc6a5ae88
alba <sfg@sfg.es>
2024-11-21 20:40:39 +0100

    Falta que el navbar sea absoluto para que nada se ponga ni delante ni detrás.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,11 +8,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
   openMenu(): void {
     const menu = document.getElementById('menu');
     if (menu) {
       menu.classList.toggle('hidden');
     }
   }
+
+  ngAfterViewInit(): void {
+  }
+
+
 }
