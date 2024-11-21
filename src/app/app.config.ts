680064e27c0e9dc2d5e903aaab8b93f9802a5e09
davidgm26 <davidgama260402@gmail.com>
2024-11-21 17:41:31 +0100

    Manejo de errores a la hora de iniciar sesion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideHttpClient,withFetch } from '@angular/common/http'
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +14,11 @@ export const appConfig: ApplicationConfig = {
     , provideAnimationsAsync(), provideAnimationsAsync(),
      provideAnimationsAsync(), provideAnimationsAsync(),
       provideAnimationsAsync(), provideAnimationsAsync(),
-    provideHttpClient(withFetch())]
+    provideHttpClient(withFetch()),provideToastr(
+      {
+        timeOut: 10000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      }
+    )]
   }
