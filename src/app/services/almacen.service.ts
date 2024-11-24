819b3310978f3c048a11ee30095520d8cb79b7cb
David Garcia Maria <davidgama260402@gmail.com>
2024-11-24 22:17:48 +0100

    Correcion minima de servicio de almacen

--- src/app/services/almacen.service.ts
+++ src/app/services/almacen.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../env/environment';
-import { EventoAlmacenResponse } from '../interfaces/evento-almacen-response';
 import { AlmacenResponse } from '../interfaces/almacen-response';
+import { EventoAlmacenResponse } from '../interfaces/evento-almacen-response';
 
 @Injectable({
   providedIn: 'root'
@@ -21,16 +21,26 @@ export class AlmacenService {
     return this.http.put(`${environment.apiUrl}/almacenes/editar/${id}`, datos);
   }
 
+  guardarAlmacen(data: AlmacenResponse): Observable<any> {
+    return this.http.post<AlmacenResponse>(`${environment.apiUrl}/guardar`, data);
+  }
+
+
   obtenerProvincias(): Observable<any[]> {
     return this.http.get<any[]>(`${environment.apiUrl}/provincias/lista`);
+  }
+
+  obtenerUsuarioPorId(id: number): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/usuarios/${id}`);
   }
 
   obtenerAlmacenesPorEventoId(eventoId: number): Observable<EventoAlmacenResponse[]> {
     return this.http.get<EventoAlmacenResponse []>(`${environment.apiUrl}/almacenes/listaregistrados/${eventoId}`);
   }
 
-  guardarAlmacen(data: AlmacenResponse): Observable<any> {
-    return this.http.post<AlmacenResponse>(`${environment.apiUrl}/almacenes/guardar`, data);
+
+  obtenerListaEventos(id: number): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/listaEventos/${id}`);
   }
 
 }
