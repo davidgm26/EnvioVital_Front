430dfd9c643ad6c8d22bcac3934eab829700574a
alba <sfg@sfg.es>
2024-11-28 15:54:01 +0100

    GuardoFiltroAlmacen.

--- src/app/components/shared/filtro-almacen-provincia/filtro-almacen-provincia.component.ts
+++ src/app/components/shared/filtro-almacen-provincia/filtro-almacen-provincia.component.ts
@@ -1,28 +1,26 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AlmacenService } from '../../../services/almacen.service';
-import { Observable } from 'rxjs';
-import { EventoAlmacenResponse } from '../../../interfaces/evento-almacen-response';
-import {FormsModule} from "@angular/forms";
 import {NgForOf} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-filtro-almacen-provincia',
-  templateUrl: './filtro-almacen-provincia.component.html',
   standalone: true,
   imports: [
-    FormsModule,
-    NgForOf
+    NgForOf,
+    ReactiveFormsModule,
+    FormsModule
   ],
+  templateUrl: './filtro-almacen-provincia.component.html',
   styleUrls: ['./filtro-almacen-provincia.component.css']
 })
 export class FiltroAlmacenProvinciaComponent implements OnInit {
   provincias: any[] = [];
   selectedProvincia: number = 0;
-  almacenes: EventoAlmacenResponse[] = [];
 
   @Output() provinciaChange = new EventEmitter<number>();
 
-  constructor(private almacenService: AlmacenService) { }
+  constructor(private almacenService: AlmacenService) {}
 
   ngOnInit(): void {
     this.almacenService.obtenerProvincias().subscribe((data: any[]) => {
@@ -31,22 +29,12 @@ export class FiltroAlmacenProvinciaComponent implements OnInit {
   }
 
   onProvinciaChange(): void {
-  this.provinciaChange.emit(this.selectedProvincia);
-  this.almacenService.obtenerAlmacenesPorEventoId(this.selectedProvincia).subscribe(
-    (data: EventoAlmacenResponse[]) => {
-      this.almacenes = data;
-    },
-    (error) => {
-      console.error('Error al obtener almacenes:', error);
-      this.almacenes = [];
-    }
-  );
-}
+    this.provinciaChange.emit(this.selectedProvincia);
+  }
 
   mostrarTodosLosAlmacenes(): void {
-    this.provinciaChange.emit(0); // Emitir 0 para indicar que se deben mostrar todos los eventos
-    this.almacenService.obtenerAlmacenesPorEventoId(0).subscribe((data: EventoAlmacenResponse[]) => {
-      this.almacenes = data;
-    });
+    this.provinciaChange.emit(0); // Emitir 0 para indicar que se deben mostrar todos los almacenes
   }
+
+
 }
